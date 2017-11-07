356cddaca33254ccc408141456472d9dfefe77a0
kentspence <kspenc05@mail.uoguelph.ca>
2017-11-07 19:27:48 +0000

    I added two lines of code involving state(we can change it later)

---
+++ A2/subject.rb
@@ -0,0 +1,15 @@
+class Subject
+   def initialize()
+      @observers = nil
+      @state = nil
+   end
+   def register_observer(observer)
+   end
+   def unregister_observer(observer)
+   end
+   def get_state()
+       return @state
+   end
+   def set_state()
+   end
+end
